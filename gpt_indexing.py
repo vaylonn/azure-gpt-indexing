3f873d14424566b916a6610d4b88312a2a6ca579
Delangle Valentin <114398794+vaylonn@users.noreply.github.com>
2023-07-27 12:00:13 +0200

    Update gpt_indexing.py

--- gpt_indexing.py
+++ gpt_indexing.py
@@ -18,18 +18,18 @@ load_dotenv('./.env')
 # Configuration de l'API OpenAI
 openai.api_type = "azure"
 openai.api_version = "2023-03-15-preview"
-openai.api_base = "https://innoopenai.openai.azure.com/"
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_base = os.environ["OPENAI_API_BASE"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 #set context window
-context_window = 2048
+context_window = 4096
 
 #set number of output tokens
 num_output = 512
 
 # Initialisation de l'objet AzureOpenAI
 # test1 représente le nom de déployment model sur Azure (le nom du modèle gpt35turbo)
-llm = AzureChatOpenAI(deployment_name="test1", temperature=0.1, max_tokens=num_output, openai_api_version=openai.api_version, model_kwargs={
+llm = AzureChatOpenAI(deployment_name="default", temperature=0.1, max_tokens=num_output, openai_api_version=openai.api_version, model_kwargs={
     "api_key": openai.api_key,
     "api_base": openai.api_base,
     "api_type": openai.api_type,
@@ -41,7 +41,7 @@ llm_predictor = LLMPredictor(llm=llm)
 embedding_llm = LangchainEmbedding(
     OpenAIEmbeddings(
         model="text-embedding-ada-002",
-        deployment="ada-test",
+        deployment="learning",
         openai_api_key= openai.api_key,
         openai_api_base=openai.api_base,
         openai_api_type=openai.api_type,
@@ -52,7 +52,7 @@ embedding_llm = LangchainEmbedding(
 
 # Chargement des documents à partir du répertoire './data'
 documents = SimpleDirectoryReader('./data').load_data()
-#permet de vérifier que les docs ont bien étés chargés
+# #permet de vérifier que les docs ont bien étés chargés
 print('Document ID:', documents[0].doc_id)
 print((f"Loaded doc n°1 with {len(documents)} pages"))
 
@@ -64,15 +64,15 @@ service_context = ServiceContext.from_defaults(
 )
 
 # Création de l'index à partir des documents et du service_context
-index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
-index.storage_context.persist(persist_dir="./storage")
-print((f"Finished building doc n°1 index with {len(index.docstore.docs)} nodes"))
+# index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+# index.storage_context.persist(persist_dir="./storage")
+# print((f"Finished building doc n°1 index with {len(index.docstore.docs)} nodes"))
 
 # Chargement de l'index à partir du stockage (commenté pour le moment)
-#from llama_index import load_index_from_storage
-#storage_context = StorageContext.from_defaults(persist_dir="./storage")
-#index = load_index_from_storage(storage_context, service_context=service_context)
-#print((f"Finished loading doc n°1 index from storage with {len(index.docstore.docs)} nodes"))
+from llama_index import load_index_from_storage
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+index = load_index_from_storage(storage_context, service_context=service_context)
+print((f"Finished loading doc n°1 index from storage with {len(index.docstore.docs)} nodes"))
 
 # Prompt de base du chatbot
 from llama_index import Prompt
@@ -81,16 +81,15 @@ template = (
     "---------------------\n"
     "{context_str}"
     "\n---------------------\n"
-    "Tu es un assistant technique de la socitété Equans via un chatbot. Tu donnes une assistance technique aux questions que te poseras l'utilisateur."
+    "Tu es un assistant technique de la socitété Equans via un chatbot qui ne parle que français. Tu donnes une assistance technique aux questions que te poseras l'utilisateur."
     "D'après le contexte, réponds à la question en français uniquement, même si la question est posée dans une autre langue. Réponds donc à la question:{query_str}\n"
-    "Il se peut que l'utilisateur te pose des questions sur des parties spécifiques du document. Essaye de les retrouver et de répondre à la question"
-    "Si la question n'a rien à voir avec les documents, réponds simplement : 'Je suis désolé, je n'ai pas pu trouver la réponse dans les documents que vous m'avez donné.' sauf si on te demande des questions sur toi(ex: Bonjour ou qui es-tu ?)."
+
 )
 qa_template = Prompt(template)
 
 # Requête envoyée au modèle
-query = "de quoi parle ce document"
-query_engine = index.as_query_engine(similarity_top_k=3, text_qa_template=qa_template)
+query = "Comment est créé le matériaux ?"
+query_engine = index.as_query_engine(similarity_top_k=3, text_qa_template=qa_template) #similarity_top_k=3
 answer = query_engine.query(query)
 
 # Affichage des résultats
